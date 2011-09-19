c14ec0445a67bdcc56881c686d2f49984e5b3046
Borisov Alexey <borisov@lcard.ru>
2011-09-19 13:32:37 +0400

    Remove timer.c -> now all timer functions is inline -> added dependence from lcspec.h. Added doxy comments

--- clock.h
+++ clock.h
@@ -1,24 +1,29 @@
-/*
- * systics.h
- *
- *  Created on: 27.08.2010
- *      Author: borisov
- */
-
-#ifndef SYSTICK_H_
-#define SYSTICK_H_
+/***************************************************************************//**
+  @addtogroup timer
+  @{
+  @file clock.h
+  Файл содержит заголовки функций для работы с системным счетчиком. Сами функции
+  реализуются портом для соответствующей платформы
+  @date 27.08.2010
+  @author Borisov Alexey <borisov@lcard.ru>
+  *****************************************************************************/
+#ifndef CLOCK_H_
+#define CLOCK_H_
 
 #include "clock_arch.h"
 
 #ifndef CLOCK_CONF_SECOND
+    /** Константа, определяющее количество отсчетов симтемного счетчика в секунду */
     #define CLOCK_CONF_SECOND 1000
 #endif
 
-
+/** Инициализация системного счетчика заданным значением и его запуск */
 void clock_init_val(t_clock init_val);
+/** Получение текущего значения системного счетчика */
 t_clock clock_time(void);
 
+/** Запуск системного счетчика в случае, если его начальное значение не важно */
 #define clock_init() clock_init_val(0);
 
 
-#endif /* SYSTICS_H_ */
+#endif /* CLOCK_H_ */
--- ports/win/clock_arch.h
+++ ports/win/clock_arch.h
@@ -1,17 +1,20 @@
-/*
- * systic_arch.h
- *
- *  Created on: 10.06.2011
- *      Author: Borisov
- */
+/***************************************************************************//**
+  @file clock_arch.c
+  Файл содержит определение типов и констант для работы с системным счетчиком
+  в OC Windows через функцию GetTickCount().
+  @date 10.06.2011
+  @author Borisov Alexey <borisov@lcard.ru>
+ ******************************************************************************/
 
-#ifndef SYSTIC_ARCH_H_
-#define SYSTIC_ARCH_H_
+#ifndef CLOCK_ARCH_H_
+#define CLOCK_ARCH_H_
 
 #include "Windows.h"
 
+/* GetTickCount() возвращает счетчик в милисекундах */
 #define CLOCK_CONF_SECOND 1000
 
-typedef int t_clock;
+/* GetTickCount() возвращает в качестве значения DWORD */
+typedef DWORD t_clock;
 
-#endif /* SYSTIC_ARCH_H_ */
+#endif /* CLOCK_ARCH_H_ */
